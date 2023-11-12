8346c5f95999ef0cc12d29ce13049e8773992679
alihssan <alyhassan862@gmail.com>
2023-11-12 23:32:23 +0500

    Chore : Added support for models and APIs

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
 from fastapi import FastAPI, HTTPException
-from databases import Database
+from sqlalchemy import create_engine, Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from models import Item
+from dotenv import load_dotenv
+import os
 
+#load variables from .env here
+load_dotenv()
+database_url = os.getenv("DATABASE_URL")
+debug_mode = os.getenv("DEBUG")
 
 app = FastAPI()
 
-DATABASE_URL = "mysql+mysqlconnector://username:password@localhost/db_name"
+#Database session maker
+engine = create_engine(database_url)
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-database = Database(DATABASE_URL)
+
+#APIs 
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.post("/items/")
-async def create_item(item: Item):
-    query = "INSERT INTO items (name, description) VALUES (:name, :description)"
-    values = {"name": item.name, "description": item.description}
-    await database.execute(query=query, values=values)
-    return {"item": item}
+async def create_item(item: Item, db: Session = get_db):
+    db.add(item)
+    db.commit()
+    db.refresh(item)
+    return item
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    query = "SELECT name, description FROM items WHERE id = :id"
-    values = {"id": item_id}
-    result = await database.fetch_one(query=query, values=values)
-    if result is None:
+async def read_item(item_id: int, db: Session = get_db):
+    item = db.query(Item).filter(Item.id == item_id).first()
+    if item is None:
         raise HTTPException(status_code=404, detail="Item not found")
-    return result
-
-
+    return item
--- models.py
+++ models.py
@@ -1,6 +1,21 @@
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
 
-class Item(BaseModel):
-    name: str
-    description: str = None
+load_dotenv()
+database_url = os.getenv("DATABASE_URL")
+Base = declarative_base()
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+
+engine = create_engine(database_url)
+Base.metadata.create_all(bind=engine)
